821307455cce977a2449fcd271a084a0f3870569
TakoArishi <tako@hey.com>
2024-03-06 09:33:31 -0600

    Re-add Tag Frames and add basic (but non-functioning) tag functionality

--- interface.py
+++ interface.py
@@ -1,13 +1,15 @@
 import tkinter as tk
-from tkinter import Tk, ttk, Canvas, Frame, Button, Label, Entry, TOP, BOTTOM, LEFT, RIGHT, BOTH, HORIZONTAL
+from tkinter import Tk, Canvas, Frame, Button, Label, Entry, simpledialog, LEFT, RIGHT, BOTH
 from PIL import ImageTk,  Image
 from tagging import TagHandler
 
 from os import listdir
 from os.path import isfile, join, basename, abspath
+from functools import partial
+
 
 class Window:
-    def __init__(self, width: int, height: int, title: str, 
+    def __init__(self, width: int, height: int, title: str,
                  tag_handler: TagHandler = None, 
                  image_dir: str = None, 
                  last_image_path: str = None):
@@ -20,6 +22,7 @@ class Window:
         self.root.title(title)
 
         # Hard-Coded Constants TODO: Move to Config File
+        self.MENU_BAR_HEIGHT = 40
         self.IMAGE_SELECT_WIDTH = 250
         self.IMAGE_SELECT_HEIGHT = 100
         self.VIEW_FRAME_HEIGHT = 450
@@ -37,6 +40,7 @@ class Window:
         # Currently-selected image
         self.primary_image_filepath = join(image_dir, last_image_path)
         self.primary_image_canvas = None
+        self.primary_image_tags = self.tag_handler.get(self.primary_image_filepath) if not None else []
         self.image_select_canvases = []
 
     def start(self):
@@ -46,26 +50,54 @@ class Window:
 
     def prepare_window(self):
         self.prepare_frames(self.width)
+        self.prepare_menu_bar_frame(self.menu_bar_frame, self.width, self.MENU_BAR_HEIGHT)
         self.prepare_image_select_frame(self.image_select_frame, self.IMAGE_SELECT_WIDTH, self.IMAGE_SELECT_HEIGHT)
         self.prepare_view_frame(self.view_frame, self.width, self.VIEW_FRAME_HEIGHT)
-        self.prepare_tag_view_frame()
-        self.prepare_tag_suggestions_frame()
-        self.prepare_tag_entry_frame()
+        self.prepare_tag_view_frame(self.tag_view_frame)
+        # self.prepare_tag_suggestions_frame()
+        self.prepare_tag_entry_frame(self.tag_entry_frame)
         self.select_image(self.primary_image_canvas, join(self.image_directory, self.primary_image_filepath))
         self.populate_image_select_frame(self.image_select_canvases, self.primary_image_filepath, self.image_paths_list)
 
     def prepare_frames(self, window_width):
-        self.image_select_frame = Frame(self.root, width = window_width, height = 50)
+        self.menu_bar_frame = Frame(self.root, width = window_width, height = self.MENU_BAR_HEIGHT)
+        self.image_select_frame = Frame(self.root, width = window_width, height = self.IMAGE_SELECT_HEIGHT)
         self.view_frame = Frame(self.root, width = window_width)
-        self.tag_view_frame = Frame(self.root, width = window_width, height = 50)
-        self.tag_suggestions_frame = Frame(self.root, width = window_width, height = 50)
+        self.tag_view_frame = Frame(self.root, width = window_width)
+        # self.tag_suggestions_frame = Frame(self.root, width = window_width, height = 50)
         self.tag_entry_frame = Frame(self.root, width = window_width, height = 50)
 
+        self.menu_bar_frame.pack(fill = BOTH, expand = True)
         self.image_select_frame.pack(fill = "x", expand = True)
         self.view_frame.pack(fill = BOTH, expand = True)
         self.tag_view_frame.pack()
-        self.tag_suggestions_frame.pack()
+        # self.tag_suggestions_frame.pack()
         self.tag_entry_frame.pack()
+
+    def prepare_menu_bar_frame(self,
+                               frame: tk.Frame,
+                               width: int,
+                               height: int):
+        menu_bar = tk.Menu(frame)
+
+        file_menu = tk.Menu(menu_bar, tearoff = 0)
+        file_menu.add_command(label = "Save Tags", command = self.on_save_tags)
+        menu_bar.add_cascade(label = "File", menu = file_menu)
+
+        search_menu = tk.Menu(menu_bar, tearoff = 0)
+        search_menu.add_command(label = "Search Tag", command = self.on_search_tags)
+        menu_bar.add_cascade(label = "Search", menu = search_menu)
+
+        self.root.config(menu = menu_bar)
+
+    def on_save_tags(self):
+        print("Saving!")
+        self.tag_handler.pack()
+        print("Done!")
+
+    def on_search_tags(self):
+        tag_to_search = simpledialog.askstring("Search Tags", "Enter a tag to search for")
+        print(tag_to_search)
 
     def prepare_image_select_frame(self,
                                    frame: tk.Frame,
@@ -162,47 +194,47 @@ class Window:
         return tk_image
 
     def prepare_view_frame(self, frame: tk.Frame, width: int, height: int):
-        # Prepare View Frame
         # Use a default value for the width and height, just in case the image isn't selected yet
-
         if not self.primary_image_canvas:
             self.primary_image_canvas = Canvas(self.view_frame, width = width, height = height)
         self.primary_image_canvas.pack(fill=BOTH, expand=True)
 
-        # Create a Canvas widget
-        canvas = tk.Canvas(frame, width=self.width, height=self.VIEW_FRAME_HEIGHT)
-        canvas.pack(side=tk.LEFT, padx=10)  # Adjust the placement as needed
-
-    def prepare_tag_view_frame(self):
+    def prepare_tag_view_frame(self, frame: tk.Frame):
         # Prepare Tag View Frame
-        tag_label = Label(self.tag_view_frame, text = "Tags: ", anchor = "w")
+        tag_label = Label(frame, text = "Tags: ", anchor = "w")
         tag_label.pack(side = LEFT)
         
-        # list_tags = self.tag_handler.get(self.primary_image_filepath)
-        # if list_tags is None:
-        #     no_tags_label = Label(self.tag_view_frame, text = "None", anchor = "e")
-        #     no_tags_label.pack(side = RIGHT)
+        if self.primary_image_tags is not None:
+            for i in range(1, len(self.primary_image_tags)):
+                tag = Button(frame, text = self.primary_image_tags[i])
+                tag.pack(side = LEFT)
 
-        # for tag_number in range(1, len(self.tag_handler.get(self.primary_image_filepath))):
-        #     tag = Button(self.tag_view_frame, text = "Recent Tag " + str(tag_number))
-        #     tag.pack(side = LEFT)
-        #     recent_tags.append(tag)
+    # def prepare_tag_suggestions_frame(self):
+    #     # Prepare Tag Suggestions Frame
+    #     tag_suggestion_label = Label(self.tag_suggestions_frame, text = "Tag Suggestions: ", anchor = "w")
+    #     tag_suggestion_label.pack(side = LEFT)
+    #     for tag_number in range(1, 11):
+    #         tag = Button(self.tag_suggestions_frame, text = "Recent Tag " + str(tag_number))
+    #         tag.pack(side = LEFT)
+    #         self.recent_tags.append(tag)
 
-    def prepare_tag_suggestions_frame(self):
-        # Prepare Tag Suggestions Frame
-        tag_suggestion_label = Label(self.tag_suggestions_frame, text = "Tag Suggestions: ", anchor = "w")
-        tag_suggestion_label.pack(side = LEFT)
-        # for tag_number in range(1, 11):
-        #     tag = Button(self.tag_suggestions_frame, text = "Recent Tag " + str(tag_number))
-        #     tag.pack(side = LEFT)
-        #     self.recent_tags.append(tag)
-
-    def prepare_tag_entry_frame(self):
+    def prepare_tag_entry_frame(self, frame: tk.Frame):
         # Prepare Entry Frame
-        tag_entry_label = Label(self.tag_entry_frame, text = "Enter New Tag: ")
-        tag_entry_label.pack(side = LEFT)
-        tag_entry = Entry(self.tag_entry_frame, width = 50)
-        tag_entry.pack()
+        tag_entry_label = Label(frame, text = "Enter New Tag: ")
+        tag_entry_label.pack(fill = "x", side = LEFT)
+
+        tag_entry = Entry(frame, width = 50)
+        tag_entry.pack(fill = "x", side = LEFT)
+
+        on_tag_image_callable = partial(self.on_tag_image, tag_entry)
+        tag_submit_button = Button(frame, text = "Submit", command = on_tag_image_callable)
+        tag_submit_button.pack(fill = "x", side = LEFT)
+
+    def on_tag_image(self, tag_entry: tk.Entry):
+        tag_text = tag_entry.get()
+        print(tag_text)
+        self.add_tag()
+        self.primary_image_tags.append(tag_text)
 
     def select_image(self, canvas: tk.Canvas, image_path: str, image_tag: str = "primary_image"):
         # Load and display the image on the canvas
