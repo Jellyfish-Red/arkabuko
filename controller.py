6a0c90d6dd11f1511b60ed9bf512e278d23439ab
TakoArishi <tako@hey.com>
2024-03-06 12:27:45 -0600

    Fix image select index out of range bug from selecting left too many times

--- controller.py
+++ controller.py
@@ -29,24 +29,23 @@ class Controller:
         self.view.tag_submit_button.configure(command = on_tag_image_command)
 
     def on_click_left_image(self, event):
-        image_path = self.view.image_select_image_list[0]
-        self.select_image(image_path)
+        self.select_image(-1)
 
     def on_click_middle_image(self, event):
-        image_path = self.view.image_select_image_list[1]
-        self.select_image(image_path)
+        self.select_image(0)
 
     def on_click_right_image(self, event):
-        image_path = self.view.image_select_image_list[2]
-        self.select_image(image_path)
+        self.select_image(1)
 
-    def select_image(self, image: tuple[str, ImageTk.PhotoImage]):
-        path = image[0]
-        print(path)
-        if path is not None or path != "":
-            self.model.update_selected_image(path)
-            self.view.display_primary_image(self.view.primary_image_canvas, path)
-            self.view.populate_image_select_frame(self.view.image_select_canvases, path, self.model.get_adjacent_images())
+    def select_image(self, offset: int):
+        if 0 <= self.model.selected_image_index + offset < len(self.model.image_paths_list):
+            image_path = self.model.image_paths_list[self.model.selected_image_index + offset]
+            print(image_path)
+            
+            if image_path is not None or image_path != "":
+                self.model.update_selected_image(image_path)
+                self.view.display_primary_image(self.view.primary_image_canvas, image_path)
+                self.view.populate_image_select_frame(self.view.image_select_canvases, image_path, self.model.get_adjacent_images())
 
     def on_tag_image(self, tag_entry: Entry):
         tag_text = tag_entry.get()
